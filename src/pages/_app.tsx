d9969f7af8f1f05c50abed315bbd9adf4e91b75b
samir <18037701129@qq.com>
2022-12-01 14:49:33 +0800

    feat：money

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,16 +22,19 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Next and MDX Blog</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <main style={{minHeight:'100vh',position:'relative'}}>
+      <main style={{ minHeight: '100vh', position: 'relative' }}>
         <div className={styles.light}></div>
-        <Nav back={router.pathname.includes('slug')?true:false} />
+        <Nav back={router.pathname.includes('slug') ? true : false} />
         <Component {...pageProps} />
         <div>
           <BubblyButton />
           <HostTitle />
         </div>
         <div></div>
-        <div className="fixed bottom-10 right-10 w-50 h-50 bg-black flex items-center justify-center rounded-full hover:bg-skin-900"><span className="iconfont icon-qiandai text-28 ">￥8</span></div>
+        <div className={styles.fixedMonety}>
+          <span className="iconfont icon-qiandai text-28 "></span>
+          <div className={styles.money}>￥14</div>
+        </div>
       </main>
     </>
   )
